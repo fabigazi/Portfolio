5a67c16596c867471f5b1929b8a59c1369052d9e
fabigazi <fgaziano98@gmail.com>
2024-03-08 22:55:13 -0500

    added assets

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,10 +1,19 @@
 import React from 'react'
 import './navbar.css';
+import { RiMenu3Line, RiCloseLine } from 'react-icons/ri';
+
+//BEM -> Block Element Modifier
 
 const Navbar = () => {
   return (
     <div>
-      Navbar
+      <div className='gpt3__navbar'>
+        <div className='gpt3__navbar-links'>
+          <dev className='gpt3__navbar-links_logo'>
+            <img src={logo} alt="logo"/>
+          </dev>
+        </div>
+      </div>
     </div>
   )
 }
