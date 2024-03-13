443db0064e8645c583e2e51f9052299e1b9ad34b
fabigazi <fgaziano98@gmail.com>
2024-03-13 13:30:41 -0400

    Possibilities CTA and Features were added

--- src/containers/features/Features.jsx
+++ src/containers/features/Features.jsx
@@ -1,10 +1,38 @@
 import React from 'react'
 import './features.css';
+import { Feature } from '../../components';
+
+const featuresData = [
+  {
+    title: 'Improving and distrusts instantly',
+    text: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
+  },
+  {
+    title: 'Become the tended active',
+    text: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
+  },
+  {
+    title: 'Message or am noting',
+    text: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
+  },
+  {
+    title: 'Really boy law county',
+    text: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
+  },
+];
 
 const Features = () => {
   return (
-    <div>
-      Features
+    <div className='gpt3__features section__padding' id='features'>
+      <div className='gpt3__features-heading'>
+        <h1 className='gradient__text'>The Future is Now and You Just Need To Relize It. Step into the Future Today & Make it Happen.</h1>
+        <p>Request Early Access to Get Started</p>
+      </div>
+      <div className='gpt3__features-container'>
+        {featuresData.map((item,index) => (
+          <Feature title={item.title} text={item.text} key={item.title + index} />
+        ))}
+      </div>
     </div>
   )
 }
--- src/containers/possibility/Possibility.jsx
+++ src/containers/possibility/Possibility.jsx
@@ -1,10 +1,20 @@
 import React from 'react'
 import './possibility.css';
 
+import possibilityImage from "../../assets/possibility.png"
+
 const Possibility = () => {
   return (
-    <div>
-      Possibility
+    <div className='gpt3__possibility section__padding' id='possibility'>
+      <div className='gpt3__possibility-image'>
+        <img src={possibilityImage} alt='possibility'/>
+      </div>
+      <div className='gpt3__possibility-content'>
+        <h4>Request Early Acess to Get Started</h4>
+        <h1 className='gradient__text'>The possibilities are beyond your imagination</h1>
+        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+        <h4>Request Early Acess to Get Started</h4>
+      </div>
     </div>
   )
 }
